415db189485272463f609b2f7dcd074452e147b1
Linnéa Wennberg <59705400+oldskool123@users.noreply.github.com>
2021-05-01 12:43:24 +0200

    Uploading project
    Added nodejsAPI with authentication JWT

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node.js API Authentication with JWT and MongoDB
+
+## Get started
+
+Clone project with `git clone`.
+
+Install dependencies with `npm install`.
+
+Install dev dependecy `npm install nodemon --save-dev `.
+
+Start project with `npm start`.
---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const { registerValidation, loginValidation } = require("../validation");
+
+router.post("/register", async (req, res) => {
+	// Validate data before creating user
+	const { error } = registerValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Check if the user exists in DB
+	const emailExist = await User.findOne({ email: req.body.email });
+	if (emailExist) return res.status(400).send("Email already exists");
+
+	// Hash passwords
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+	const user = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: hashedPassword,
+	});
+	try {
+		const savedUser = await user.save();
+		// res.send(savedUser);
+		res.send({ user: user._id });
+	} catch (err) {
+		res.status(400).send(err);
+	}
+});
+
+// Login
+router.post("/login", async (req, res) => {
+	// Validate data before creating user
+	const { error } = loginValidation(req.body);
+	if (error) return res.status(400).send(error.details[0].message);
+
+	// Check if the email exists
+	const user = await User.findOne({ email: req.body.email });
+	if (!user) return res.status(400).send("Email doesn't exist");
+
+	// Check if password is correct
+	const validPass = await bcrypt.compare(req.body.password, user.password);
+	if (!validPass) return res.status(400).send("Invalid password");
+
+	// Create and assign a token
+	const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+	res.header("auth-token", token).send(token);
+
+	//res.send("Logged in!");
+});
+
+module.exports = router;
